e1fd664f0082df02651fa1e777c893bda00fa3b8
Jacopo Soldani <jsoldani@gmail.com>
2021-01-25 15:45:41 +0100

    first version

---
+++ src/main/java/cybersec/cloud/biblioteca/BibliotecaApp.java
@@ -0,0 +1,19 @@
+package cybersec.cloud.biblioteca;
+
+import io.dropwizard.Application;
+import io.dropwizard.setup.Environment;
+
+public class BibliotecaApp extends Application<BibliotecaConfig> {
+    
+    public static void main(String[] args) throws Exception {
+        new BibliotecaApp().run(args);
+    }
+    
+    @Override
+    public void run (BibliotecaConfig configuration, Environment environment) {
+        final Biblioteca risorsaBiblioteca = 
+            new Biblioteca(configuration.getDescrizionePredefinita());
+        environment.jersey().register(risorsaBiblioteca);
+    }
+
+}
---
+++ src/main/java/cybersec/cloud/biblioteca/BibliotecaConfig.java
@@ -0,0 +1,16 @@
+package cybersec.cloud.biblioteca;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import io.dropwizard.Configuration;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class BibliotecaConfig extends Configuration {
+    
+    private String descrizionePredefinita;
+    
+    @JsonProperty
+    public String getDescrizionePredefinita() {
+        return this.descrizionePredefinita;
+    }
+    
+}
